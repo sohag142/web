631e642dd2d9c13892c025ea8faf6543dee78d57
Sohag <134754736+sohag142@users.noreply.github.com>
2025-06-17 11:06:09 +0600

    Add files via upload

---
+++ v_55/scripts.js
@@ -0,0 +1,20 @@
+console.log("Hey this is video 55")
+
+var a = 5;
+var b = 6;
+var c = "sohag"
+
+console.log(a +b + 8);
+console.log(typeof a , typeof b , typeof c);
+
+let o = {
+    "Name": " sohag",
+    "job role": 5600,
+    "is hand": "ture"
+}
+
+console.log(o);
+o.salary = "100k";
+console.log(o);
+o.salary = "500k";
+console.log(o);
