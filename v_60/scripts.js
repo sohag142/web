c79f7ee226e969dd25ff40b6567f5321f1ab3128
Sohag <134754736+sohag142@users.noreply.github.com>
2025-06-18 14:02:20 +0600

    Add files via upload

---
+++ v_60/scripts.js
@@ -0,0 +1,27 @@
+console.log("This is string tutorials")
+
+let a = " sohag";
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+
+console.log(a.length)
+
+let name = "sohag ali"
+let friend = " rohan "
+console.log("His name is " + name + " and His friend name is " + friend)
+console.log(`His name is ${name}`)
+ let b = " Rezwan Sarkar"
+ console.log(b.toUpperCase())
+ console.log(b.toLowerCase())
+ console.log(b.length)
+ console.log(b.slice(1,9))
+ console.log(b.replace("Re", "77"))
+ console.log(b.concat(a, " sumi"))
+
+ console.log(b)
+
+ console.log(b.charAt(3))
